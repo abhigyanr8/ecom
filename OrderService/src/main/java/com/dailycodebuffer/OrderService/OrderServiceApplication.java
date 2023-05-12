cd5e8cabfb9b455f2fc01775f3a97b2c3396748b
Abhigyan <abhigyanr8@gmail.com>
2023-05-12 16:43:51 +0530

    okta auth completed

--- OrderService/src/main/java/com/dailycodebuffer/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.dailycodebuffer.OrderService;
 
+import com.dailycodebuffer.OrderService.external.intercept.RestTemplateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -10,8 +12,11 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -22,17 +27,30 @@ public class OrderServiceApplication
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+
+	@Autowired
+	private ClientRegistrationRepository clientRegistrationRepository;
+	@Autowired
+	private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate()
 	{
-		return new RestTemplate();
+		RestTemplate restTemplate =  new RestTemplate();
+		restTemplate.setInterceptors(Arrays.asList(new RestTemplateInterceptor(
+				clientManager(clientRegistrationRepository,oAuth2AuthorizedClientRepository)
+		)));
+		return restTemplate;
 	}
-//    @Bean
-//    public OAuth2AuthorizedClientManager clientManager(ClientRegistrationRepository clientRegistrationRepository,
-//													   OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository)
-//	{
-//		OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
-//
-//	}
+    @Bean
+    public OAuth2AuthorizedClientManager clientManager(ClientRegistrationRepository clientRegistrationRepository,
+													   OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository)
+	{
+		OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+		DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+				clientRegistrationRepository,
+				oAuth2AuthorizedClientRepository
+		);
+		return oAuth2AuthorizedClientManager;
+	}
 }
---
+++ PyamentService/src/main/java/com/dailycodebuffer/PyamentService/security/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.dailycodebuffer.PyamentService.security;
+
+import org.springframework.context.annotation.Bean;
+
+public class WebSecurityConfig
+{
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+//        http
+//                .authorizeRequests(authorizeRequests -> authorizeRequests)
+//                .antMatchers("/payment/**")
+//                .hasAuthority("SCOPE_internal")
+//                .anyRequest()
+//                .authenticated()
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+//        return http.build();
+//    }
+}
